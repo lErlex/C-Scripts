971b5332469d78b2943df2bd7cd49895421bec05
lErlex <59973954+lErlex@users.noreply.github.com>
2022-03-24 20:26:30 +0100

    Generator of random names
    This script generate a random character string with a custom length.

---
+++ generate.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<time.h>
+#include<stdlib.h>
+#include <cstdlib>
+#include<string.h>
+
+using namespace std;
+
+int randomized(){
+	int rnum = 65 + rand()%(90-65);
+	return rnum;
+}
+
+int main() {
+	srand((unsigned)time(0));
+	
+	int caracters;
+	string name = "", random = "";
+	
+	cout << "Cuantos caracteres quieres ";
+	cin >> caracters;
+	
+	for(int i = 0; i < 10; i++){
+		for(int i = 0; i < caracters; i++){
+			random = randomized();
+			name = name + random;
+		}
+		cout << name << endl;
+		name = "";
+	}
+	return 0; 
+}
